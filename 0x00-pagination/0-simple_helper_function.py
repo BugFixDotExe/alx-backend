49b7391f0f8123b6ae01ecfa0d01a63bdec849c7
BugfixDotexe <bugfix.exe@gmail.com>
2024-10-30 08:45:48 +0100

    debugged problem 0 had too little documentation

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,6 +9,14 @@ return a tuple
 
 
 def index_range(page: int, page_size: int) -> tuple:
+    '''
+    index_range: a function that returns a tuple of start and end index
+    Args:
+        page: the page of type int
+        page_size: the page size of type int
+    Returns:
+        A tuple containg the calculated page start and index indexs
+    '''
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
