ebeae3b5623f70aa85340863062ac5f96edb5c73
BugFixDotExe <bugfix.exe@gmail.com>
2024-12-02 13:52:19 +0100

    solution to problem 1-redis

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -2,11 +2,21 @@ import redis from 'redis';
 
 const client = redis.createClient();
 
-client.on('connect', () => console.log('Redis client connected to the server'));
-client.on('error', err => console.log(`Redis client not connected to the server: ${err}`));
+const redisConnect = new Promise((resolve, reject) => { 
+  client.on('connect', () => {
+    console.log('Redis client connected to the server');
+      resolve();
+  });
+  client.on('error', err => {
+    console.log(`Redis client not connected to the server: ${err}`)
+    reject(err);
+  });
+})
 
-function setNewSchool(schoolName, value) { 
-  client.set(schoolName, value);
+function setNewSchool(schoolName, value) {
+  redisConnect.then(() => { 
+    client.set(schoolName, value, redis.print);
+  })
 }
 
 function displaySchoolValue(schoolName) { 
