4b31daa00592c16ef41516cce1725aeae5d406ec
BugFixDotExe <bugfix.exe@gmail.com>
2024-12-04 11:56:32 +0100

    solution to pub/sub

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,6 +1,8 @@
 import redis from 'redis';
 const client = redis.createClient();
 
+let flag = false;
+
 const redisConnect = new Promise((resolve, reject) => {
   client.on('connect', () => {
     console.log('Redis client connected to the server');
@@ -16,10 +18,12 @@ redisConnect.then(() => {
   client.subscribe('holberton school channel');
   client.on('message', (channel, message) => {
     if (message === "KILL_SERVER") {
-      console.log(message); 
+      flag = true;
+      console.log(message);
       client.unsubscribe('holberton school channel');
       client.quit();  // Gracefully close the connection
     } else {
+      if (flag) {return}
       console.log(message);  // Handle other messages
     }
   });
